76d8240675d161b5092e646ec077560e5a870d9f
gimlim1 <ricardojosepineda@hotmail.com>
2015-09-26 17:15:02 -0600

    Pruebas Unitarias

---
+++ ConsoleApplication1/ResolverEcuacionCuadratica.Test/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ResolverEcuacionCuadratica;
+
+namespace ResolverEcuacionCuadratica.Test
+{
+    /*
+    Declare variables para los parámetros reales
+    Declare variables para los resultados reales
+    Declare variables para los resultados esperados
+    Construya la lógicas que realiza la prueba
+    Asegúrese de que los valores reales son iguales a los esperados.    
+   */
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ProbarResolverCorrectoExacto()
+        {
+            double primer = -1;
+            double segundo = 2;
+            double tercer = 3;
+            double primeraRaizEsperada = 3;
+            double segundaRaizEsperada = -1;
+            clsParametrosEcuacion parametros = new clsParametrosEcuacion(primer,segundo,tercer);
+
+            ResolverEcuacionCuadratica resolver = new ResolverEcuacionCuadratica();
+            //invoque al método correspondiente
+            resolver.ResolverLaEcuacionCuadratica(parametros);
+            
+
+            Assert.IsTrue((primeraRaizEsperada == parametros.getPrimerRaiz() || 
+                           primeraRaizEsperada == parametros.getSegundRaiz()));
+
+            Assert.IsTrue((segundaRaizEsperada == parametros.getPrimerRaiz() ||
+                           segundaRaizEsperada == parametros.getSegundRaiz()));
+
+        }
+    }
+}
+/* 
+        tarea moral:
+        cree pruebas unitarias para los siguientes casos:
+        - Pruebas correctas para resultados reales con muchos decimales, 
+          de modo que la coincidencia NO sea exacta y se apruebe 
+          cuando hay coincidan al menos en el sexto decimal
+        - pruebas correctas para cuando el discriminante es negativo
+        - pruebas para cuando el valor de a (primerNumero) es igual a cero (0)
+        */
+
