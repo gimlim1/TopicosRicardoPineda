9354f52d348dc6e4674d99f4229ecbecc8643ea1
gimlim1 <ricardojosepineda@hotmail.com>
2015-09-26 23:10:19 -0600

    Tarea 2

--- ConsoleApplication1/ResolverEcuacionCuadratica.Test/UnitTest1.cs
+++ ConsoleApplication1/ResolverEcuacionCuadratica.Test/UnitTest1.cs
@@ -42,30 +42,37 @@ namespace ResolverEcuacionCuadratica.Test
             double primer = 0;
             double segundo = 2;
             double tercer = 3;
-            double primeraRaizEsperada = 3;
-            double segundaRaizEsperada = -1;
+
             clsParametrosEcuacion parametros = new clsParametrosEcuacion(primer, segundo, tercer);
 
-            ResolverEcuacionCuadratica resolver = new ResolverEcuacionCuadratica();
+            Assert.IsTrue(parametros.getPrimer() == 0);
+         
+       } 
+
+        [TestMethod]
+        public void ProbarDiscriminante()
+        {
+            double primer = 3;
+            double segundo = 2;
+            double tercer = 1;
+
+            clsParametrosEcuacion parametros = new clsParametrosEcuacion(primer, segundo, tercer);
+            CalcularDiscriminante discri = new CalcularDiscriminante();
+
+            discri.CalcularElDiscriminante(parametros);
+
+            ValidarDiscriminante validar = new ValidarDiscriminante();
             //invoque al método correspondiente
-            resolver.ResolverLaEcuacionCuadratica(parametros);
-
-            Assert.IsTrue((primeraRaizEsperada == parametros.getPrimerRaiz() ||
-                           primeraRaizEsperada == parametros.getSegundRaiz()));
-
-            Assert.IsTrue((segundaRaizEsperada == parametros.getPrimerRaiz() ||
-                           segundaRaizEsperada == parametros.getSegundRaiz()));
-
+            Assert.IsFalse(validar.ValidarElDiscriminanteMayorQueCero(parametros.getDiscriminante()));
+          
         }
-    }
 }
-/* 
+
+}
+     /* 
         tarea moral:
         cree pruebas unitarias para los siguientes casos:
-        - Pruebas correctas para resultados reales con muchos decimales, 
-          de modo que la coincidencia NO sea exacta y se apruebe 
-          cuando hay coincidan al menos en el sexto decimal
         - pruebas correctas para cuando el discriminante es negativo
         - pruebas para cuando el valor de a (primerNumero) es igual a cero (0)
-        */
+      */
 
